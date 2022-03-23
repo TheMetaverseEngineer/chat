d6a3407b450331f2e8ac58187ed3d113030ec8f7
Saeed <saeecl@gmail.com>
2022-03-23 20:24:31 +0430

    Error Handle

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import Logo from "../assets/logo.svg";
 import {ToastContainer , toast} from "react-toastify";
-
+import "react-toastify/dist/ReactToastify.css"
 
 function Register() {
 
