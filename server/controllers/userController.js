455012431594038e887583b030d320170cf6f712
Saeed <saeecl@gmail.com>
2022-04-02 15:04:37 +0430

    update login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -24,3 +24,22 @@ module.exports.register = async (req , res, next) => {
      next(ex);
    }
 };
+
+  module.exports.login = async (req , res, next) => {
+
+  try {
+   const {username , password } = req.body;
+   const user = await User.findOne({ username });
+   if(!user)
+     return res.json({msg:"Incorrect Username" , status: false });
+   const isPasswordValid = await brcypt.compare(password, user.password);
+   if(!isPasswordValid)
+     return res.json({msg:"Incorrect Password" , status: false });
+    delete user.password;
+
+
+    return res.json({  status: true, user});
+  } catch (ex) {
+    next(ex);
+  }
+};
