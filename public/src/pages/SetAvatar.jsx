2569ec31d5c5bf9afaa5698ffddd434114e6823e
Saeed <saeecl@gmail.com>
2022-04-05 21:23:46 +0430

    set avatar to user

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -6,7 +6,7 @@ import {ToastContainer , toast} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"
 import axios from "axios";
 import { setAvatarRoute } from '../utils/APIRoutes';
-
+import { Buffer } from 'buffer';
 
 export default function SetAvatar() {
 
@@ -25,7 +25,34 @@ export default function SetAvatar() {
             theme: "dark",
 
         };
-    const setProfilePicture = async () => {} ;
+
+
+    useEffect(async ()=> {
+        if (!localStorage.getItem("chat-app-user")){    
+            navigate("/login")
+
+        }
+    },[]);    
+    const setProfilePicture = async () => {
+        if(selectedAvatar===undefined) {
+            toast.error("Please Select Your Avarar",toastOptions);
+
+        } else {
+            const user = await JSON.parse(localStorage.getItem("chat-app-user"));
+            const { data } = await axios.post(`${setAvatarRoute}/${user._id}` , {
+                image: avatars[selectedAvatar],
+            });
+            if(data.isSet) {
+                user.isAvatarImageSet = true;
+                user.avatarImage = data.image ;
+                localStorage.setItem("chat-app-user", JSON.stringify(user));
+                navigate('/');
+            } else {
+                toast.error("Error , Please Try Again", toastOptions);
+            }
+        }
+
+    } ;
     useEffect(async ()=> {
         const data = [];
         for (let i=0; i<4 ; i++ ) {
@@ -43,6 +70,15 @@ export default function SetAvatar() {
 
   return (
   <>
+
+
+  {
+      isLoading ? <Container>
+          <img src={loader} alt="loader" className="loader" /> <span>Loading...</span>
+          
+               </Container> : (
+
+              
   <Container>
       <div className="title-container">
           <h1>
@@ -65,11 +101,68 @@ export default function SetAvatar() {
       }
          
       </div>
-      </Container>;
+      <button className='submit-btn' onClick={setProfilePicture}>Set profile Picture</button>
+      </Container> )
+  }
   <ToastContainer />
   </>
   );
  
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+
+display:flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+gap: 3rem;
+background-color: #131320;
+height: 100vh;
+width: 100vw;
+.loader {
+    max-inline-size: 100%;
+}
+.title-container {
+    h1 {
+        color: purple;
+
+    }
+}
+    .avatars {
+        display : fle;
+        gap : 2rem;
+        .avatar {
+            border : 0.5rem solid transparent ;
+            padding: 0.4rem;
+            border-radius: 5rem;
+            display: flex;
+            justify-content: center;
+            transition: 0.5s ease-in-out;
+            img {
+                height : 6rem;
+            }
+            
+        }
+        
+    .selected {
+            border : 0.5rem solid #4e0eff;
+        }
+    }
+}
+.submit-btn {
+    
+    background-color : #997af0;
+    color : white;
+    padding : 1rem 2rem;
+    border : none;
+    font-weight : bold ;
+    cursor : pointer ; 
+    border-radius : 0.4rem;
+    font-size : 1rem;
+    transition : 0.5s ease-in-out;
+    :hover {
+        background-color: #4e0eff;
+    }
+
+`;
